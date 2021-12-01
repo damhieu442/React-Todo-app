e2a94426287e119307ee54abe0e4a117476620d9
Hieu Dam <hieuvipvt1@gmail.com>
2021-12-01 09:51:44 +0700

    Finish

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './componments/TodoList';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="Todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
